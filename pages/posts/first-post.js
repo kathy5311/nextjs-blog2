257447b8eb5abb7f3f89e3795d7f62f455ece9fc
Jeong Yun Seo <kathy531@hanyang.ac.kr>
2023-04-09 15:10:31 +0900

    cse102 Assignment2

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+	return (
+	  <> 
+	    <h1>Yunseo's Workout Diary</h1>
+          <h2>
+            <Link href="/">Back to home</Link>
+          </h2>
+        </>
+	);
+}
